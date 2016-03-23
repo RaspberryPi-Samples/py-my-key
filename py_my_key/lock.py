f93fa2004a5476f29aa3bdf28848a744c95fd647
lwalter86 <lionel.walter@univ-poitiers.fr>
2016-03-23 16:56:29 +0100

    renaming

--- py_my_key/lock.py
+++ py_my_key/lock.py
@@ -13,17 +13,17 @@ logger = logging.getLogger(__name__)
 class Lock(object):
     """A lock object (serrure)"""
 
-    def __init__(self, pin, lit_state=pingo.HIGH, time_opened=5):
-        """Set lit_state to pingo.LOW to turn on led by bringing
+    def __init__(self, pin, opened_state=pingo.HIGH, time_opened=5):
+        """Set opened_state to pingo.LOW to turn on led by bringing
            cathode to low state.
 
-        :param lit_state: use pingo.HI for anode control, pingo.LOW
+        :param opened_state: use pingo.HI for anode control, pingo.LOW
                           for cathode control
         """
 
         pin.mode = pingo.OUT
         self.pin = pin
-        self.lit_state = lit_state
+        self.opened_state = opened_state
         self.time_opened = time_opened
 
     def open_and_close(self, session, reader_id, card):
@@ -39,23 +39,23 @@ class Lock(object):
         session.commit()
 
     def _open(self):
-        if self.lit_state == pingo.HIGH:
+        if self.opened_state == pingo.HIGH:
             self.pin.high()
         else:
             self.pin.low()
 
     def _close(self):
-        if self.lit_state == pingo.HIGH:
+        if self.opened_state == pingo.HIGH:
             self.pin.low()
         else:
             self.pin.high()
 
     @property
-    def lit(self):
-        return self.pin.state == self.lit_state
+    def opened(self):
+        return self.pin.state == self.opened_state
 
-    @lit.setter
-    def lit(self, new_state):
+    @opened.setter
+    def opened(self, new_state):
         if new_state:
             self.on()
         else:
